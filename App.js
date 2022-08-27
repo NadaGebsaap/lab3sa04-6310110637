e34620b0afd8a3a2bf1c67292f4aa5c9eb19561c
Nada Gebsaap <nada.gebsaap@gmail.com>
2022-08-27 20:52:51 +0700

    part2

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import Constants from 'expo-constants';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Weather from './components/Weather';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Weather zipCode="90110"/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingToo: Constants.StatusBarHeight
   },
 });
